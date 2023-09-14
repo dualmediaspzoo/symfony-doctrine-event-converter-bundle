d0e22676263666440fc6c561c15bcaea313c21bb
pkly <kakolp95@gmail.com>
2023-09-14 11:38:46 +0200

    cs fix

--- src/Proxy/Generator.php
+++ src/Proxy/Generator.php
@@ -29,7 +29,7 @@ EOF;
         'WARNING! Proxy class generated automatically, Do not modify!',
         '',
         'Event class for event <event>',
-        '@final'
+        '@final',
     ];
 
     public function __construct(
